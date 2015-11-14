962210d55b846fdb343c566689f903347499382e
zhantong1994 <zhantong1994@163.com>
2015-11-14 09:09:35 +0800

    增加异常处理
    自定义NotFound异常，在任何时候判断出当前图片找不到二维码，则抛出异常

---
+++ SCREEN/src/NotFoundException.java
@@ -0,0 +1,15 @@
+/**
+ * Created by zhantong on 15/11/13.
+ */
+public class NotFoundException extends Exception{
+    private static final NotFoundException INSTANCE=new NotFoundException();
+    protected static final StackTraceElement[] NO_TRACE=new StackTraceElement[0];
+    static {
+        INSTANCE.setStackTrace(NO_TRACE);
+    }
+    private NotFoundException(){
+    }
+    public static NotFoundException getNotFoundInstance(){
+        return INSTANCE;
+    }
+}
