a5114e3f1938167e9dfec3334a652b278d48bcf2
zhantong1994 <zhantong1994@163.com>
2015-11-30 10:21:29 +0800

    修改填充最后一张图片的方法
    将之前的填充白色修改为填充黑色

--- SCREEN/src/FileToImg.java
+++ SCREEN/src/FileToImg.java
@@ -7,14 +7,14 @@ import java.io.*;
  * Created by zhantong on 15/11/11.
  */
 public class FileToImg {
-    int frameWhiteLength=10;
+    int frameWhiteLength=8;
     int frameBlackLength=1;
     int frameVaryLength=1;
-    int contentLength=52;
-    int blockLength=4;
+    int contentLength=76;
+    int blockLength=10;
     public static void main(String[] args){
         FileToImg f=new FileToImg();
-        f.toImage(f.readFile("/Users/zhantong/Desktop/test1.txt"),"/Users/zhantong/Desktop/test2/");
+        f.toImage(f.readFile("/Users/zhantong/Downloads/docs-pdf/howto-pyporting.pdf"),"/Users/zhantong/Desktop/test7/");
     }
     public String readFile(String path){
         File inFile=new File(path);
@@ -25,9 +25,11 @@ public class FileToImg {
             e.printStackTrace();
         }
         StringBuffer stringBuffer=new StringBuffer();
+        int total_length=0;
+        int byte_length=0;
         try{
             int i;
-            int length=300;
+            int length=684;
             int ecNum=38;
             byte[] b=new byte[length];
             int[] c=new int[length+ecNum];
@@ -35,16 +37,18 @@ public class FileToImg {
             Boolean flag=false;
             while(true){
                 if((i=fileInputStream.read(b))!=length){
-                    b[i++]=1;
+                    b[i++]=-128;
                     for(int index=i;index<length;index++){
                         b[index]=0;
                     }
                     flag=true;
                 }
+                byte_length+=i;
+                total_length+=i*8;
                 for(int j=0;j<length;j++){
                     c[j]=b[j]&0xff;
                 }
-                //encoder.encode(c,ecNum);
+                encoder.encode(c,ecNum);
                 for(int k:c){
                     String s = Integer.toBinaryString(k);
                     int temp=Integer.parseInt(s);
@@ -62,6 +66,8 @@ public class FileToImg {
                 //System.out.println(i);
             }
             */
+            System.out.println("total length:"+total_length);
+            System.out.println("byte length:"+byte_length);
             return stringBuffer.toString();
         }catch (IOException e){
             e.printStackTrace();
