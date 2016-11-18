95f14cefe222acb25012380013dde8c917ec60e9
Tong ZHAN <zhantong1994@163.com>
2016-11-18 18:49:05 +0800

    给Zone添加不同情况下的方法 降低耦合，增加灵活性。

--- src/Zone.java
+++ src/Zone.java
@@ -5,42 +5,51 @@ import java.io.IOException;
  * Created by zhantong on 2016/11/17.
  */
 public class Zone {
-    private Block defaultBlock;
-    private int blockWidthInPixel;
-    private int blockHeightInPixel;
     public int widthInBlock;
     public int heightInBlock;
     public int baseOffsetInBlockX;
     public int baseOffsetInBlockY;
-    public static void main(String[] args){
-        Zone zone=new Zone(10,10,new BlackWhiteBlock(),4,4,1,1);
-        Image image=new Image((zone.widthInBlock+2)*zone.blockWidthInPixel,(zone.heightInBlock+2)*zone.blockHeightInPixel, BufferedImage.TYPE_INT_RGB);
-        int[] content={1,2,3};
-        BitContent bitContent=new BitContent(Utils.intArrayToBitSet(content,8));
-        zone.fillZone(image,bitContent);
-        try {
-            image.save("png","test.png");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private BitContent content;
+    private Block block;
+    public Zone(int widthInBlock, int heightInBlock,int baseOffsetInBlockX,int baseOffsetInBlockY,Block block){
+        this(widthInBlock,heightInBlock,baseOffsetInBlockX,baseOffsetInBlockY,block,null);
     }
-    public Zone(int widthInBlock, int heightInBlock, Block defaultBlock,int blockWidthInPixel, int blockHeightInPixel,int baseOffsetInBlockX,int baseOffsetInBlockY){
-        this.defaultBlock=defaultBlock;
-        this.blockWidthInPixel = blockWidthInPixel;
-        this.blockHeightInPixel = blockHeightInPixel;
+    public Zone(int widthInBlock, int heightInBlock,int baseOffsetInBlockX,int baseOffsetInBlockY,BitContent content){
+        this(widthInBlock,heightInBlock,baseOffsetInBlockX,baseOffsetInBlockY,null,content);
+    }
+    public Zone(int widthInBlock, int heightInBlock,int baseOffsetInBlockX,int baseOffsetInBlockY){
+        this(widthInBlock,heightInBlock,baseOffsetInBlockX,baseOffsetInBlockY,null,null);
+    }
+    public Zone(int widthInBlock, int heightInBlock,int baseOffsetInBlockX,int baseOffsetInBlockY,Block block,BitContent content){
         this.widthInBlock=widthInBlock;
         this.heightInBlock=heightInBlock;
         this.baseOffsetInBlockX=baseOffsetInBlockX;
         this.baseOffsetInBlockY=baseOffsetInBlockY;
+        this.block=block;
+        this.content=content;
     }
-    public void fillZone(Image image, BitContent content){
+    public void addContent(BitContent content){
+        this.content=content;
+    }
+    public void addBlock(Block block){
+        this.block=block;
+    }
+    public BitContent getContent(){
+        return content;
+    }
+    public void toImage(Image image,int blockLengthInPixel){
+        toImage(image,block,blockLengthInPixel);
+    }
+    public void toImage(Image image,Block block,int blockLengthInPixel){
         int pos=0;
-        int bitsPerUnit=defaultBlock.getBitsPerUnit();
+        int blockWidthInPixel=blockLengthInPixel;
+        int blockHeightInPixel=blockLengthInPixel;
+        int bitsPerUnit=block.getBitsPerUnit();
         for(int y=0;y<heightInBlock;y++){
             for(int x=0;x<widthInBlock;x++){
                 int value=content.get(pos,bitsPerUnit);
                 pos+=bitsPerUnit;
-                defaultBlock.draw(image,(baseOffsetInBlockX+x)* blockWidthInPixel,(baseOffsetInBlockY+y)* blockHeightInPixel, blockWidthInPixel, blockHeightInPixel,value);
+                block.draw(image,(baseOffsetInBlockX+x)* blockWidthInPixel,(baseOffsetInBlockY+y)* blockHeightInPixel, blockWidthInPixel, blockHeightInPixel,value);
             }
         }
     }
