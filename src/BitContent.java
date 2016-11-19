b1fd5e45c605ac320ee06a64f66d7b0e6c784aaa
Tong ZHAN <zhantong1994@163.com>
2016-11-19 10:28:41 +0800

    BitContent加入全零和全一的选项 为满足一些特定情况下的要求

--- src/BitContent.java
+++ src/BitContent.java
@@ -5,11 +5,34 @@ import java.util.BitSet;
  */
 public class BitContent {
     private BitSet content;
+    public static final int ALL_ONES=0;
+    public static final int ALL_ZEROS=1;
+    private boolean isAllOnes=false;
+    private boolean isAllZeros=false;
+    public BitContent(int type){
+        switch (type){
+            case ALL_ONES:
+                isAllOnes=true;
+                break;
+            case ALL_ZEROS:
+                isAllZeros=true;
+                break;
+        }
+    }
     public BitContent(BitSet content){
         this.content=content;
     }
     public int get(int pos,int length){
-        int value= Utils.bitsToInt(content,length,pos);
-        return value;
+        if(content!=null) {
+            int value = Utils.bitsToInt(content, length, pos);
+            return value;
+        }
+        if(isAllOnes){
+            return 1;
+        }
+        if(isAllZeros){
+            return 0;
+        }
+        return 0;
     }
 }
