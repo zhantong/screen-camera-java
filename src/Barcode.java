0cad224b37c4a3ae963664b7b6ec926c85dc0f8b
Tong ZHAN <zhantong1994@163.com>
2017-03-20 09:21:18 +0800

    增加meta层

--- src/Barcode.java
+++ src/Barcode.java
@@ -46,6 +46,7 @@ public class Barcode {
         int MARGIN=Districts.MARGIN;
         int BORDER=Districts.BORDER;
         int PADDING=Districts.PADDING;
+        int META=Districts.META;
         int MAIN_DISTRICT=Districts.MAIN;
 
         int LEFT=District.LEFT;
@@ -62,10 +63,10 @@ public class Barcode {
                 0,
                 0));
         districts.get(MARGIN).set(LEFT,new Zone(config.marginLength.get(District.LEFT),
-                config.borderLength.get(District.UP)+config.paddingLength.get(District.UP)+config.mainHeight+config.paddingLength.get(District.DOWN)+config.borderLength.get(District.DOWN),
+                config.borderLength.get(District.UP)+config.paddingLength.get(District.UP)+config.metaLength.get(District.UP)+config.mainHeight+config.metaLength.get(District.DOWN)+config.paddingLength.get(District.DOWN)+config.borderLength.get(District.DOWN),
                 districts.get(MARGIN).get(LEFT_UP).startInBlockX(),
                 districts.get(MARGIN).get(LEFT_UP).endInBlockY()));
-        districts.get(MARGIN).set(UP,new Zone(config.borderLength.get(District.LEFT)+config.paddingLength.get(District.LEFT)+config.mainWidth+config.paddingLength.get(District.RIGHT)+config.borderLength.get(District.RIGHT),
+        districts.get(MARGIN).set(UP,new Zone(config.borderLength.get(District.LEFT)+config.paddingLength.get(District.LEFT)+config.metaLength.get(District.LEFT)+config.mainWidth+config.metaLength.get(District.RIGHT)+config.paddingLength.get(District.RIGHT)+config.borderLength.get(District.RIGHT),
                 config.marginLength.get(District.UP),
                 districts.get(MARGIN).get(LEFT_UP).endInBlockX(),
                 districts.get(MARGIN).get(LEFT_UP).startInBlockY()));
@@ -94,12 +95,12 @@ public class Barcode {
                 config.borderLength.get(District.UP),
                 districts.get(MARGIN).get(LEFT).endInBlockX(),
                 districts.get(MARGIN).get(UP).endInBlockY()));
-        districts.get(BORDER).set(UP,new Zone(config.paddingLength.get(District.RIGHT)+config.mainWidth+config.paddingLength.get(District.RIGHT),
+        districts.get(BORDER).set(UP,new Zone(config.paddingLength.get(District.LEFT)+config.metaLength.get(District.LEFT)+config.mainWidth+config.metaLength.get(District.RIGHT)+config.paddingLength.get(District.RIGHT),
                 config.borderLength.get(District.UP),
                 districts.get(BORDER).get(LEFT_UP).endInBlockX(),
                 districts.get(BORDER).get(LEFT_UP).startInBlockY()));
         districts.get(BORDER).set(LEFT,new Zone(config.borderLength.get(District.LEFT),
-                config.paddingLength.get(District.UP)+config.mainHeight+config.paddingLength.get(District.DOWN),
+                config.paddingLength.get(District.UP)+config.metaLength.get(District.UP)+config.mainHeight+config.metaLength.get(District.DOWN)+config.paddingLength.get(District.DOWN),
                 districts.get(BORDER).get(LEFT_UP).startInBlockX(),
                 districts.get(BORDER).get(LEFT_UP).endInBlockY()));
         districts.get(BORDER).set(LEFT_DOWN,new Zone(config.borderLength.get(District.LEFT),
@@ -123,19 +124,19 @@ public class Barcode {
                 districts.get(BORDER).get(RIGHT).startInBlockX(),
                 districts.get(BORDER).get(RIGHT).endInBlockY()));
 
-        districts.get(PADDING).set(LEFT_UP,new Zone(config.paddingLength.get(District.RIGHT),
+        districts.get(PADDING).set(LEFT_UP,new Zone(config.paddingLength.get(District.LEFT),
                 config.paddingLength.get(District.UP),
                 districts.get(BORDER).get(LEFT).endInBlockX(),
                 districts.get(BORDER).get(UP).endInBlockY()));
-        districts.get(PADDING).set(UP,new Zone(config.mainWidth,
+        districts.get(PADDING).set(UP,new Zone(config.metaLength.get(District.LEFT)+config.mainWidth+config.metaLength.get(District.RIGHT),
                 config.paddingLength.get(District.UP),
                 districts.get(PADDING).get(LEFT_UP).endInBlockX(),
                 districts.get(PADDING).get(LEFT_UP).startInBlockY()));
-        districts.get(PADDING).set(LEFT,new Zone(config.paddingLength.get(District.RIGHT),
-                config.mainHeight,
+        districts.get(PADDING).set(LEFT,new Zone(config.paddingLength.get(District.LEFT),
+                config.metaLength.get(District.UP)+config.mainHeight+config.metaLength.get(District.DOWN),
                 districts.get(PADDING).get(LEFT_UP).startInBlockX(),
                 districts.get(PADDING).get(LEFT_UP).endInBlockY()));
-        districts.get(PADDING).set(LEFT_DOWN,new Zone(config.paddingLength.get(District.RIGHT),
+        districts.get(PADDING).set(LEFT_DOWN,new Zone(config.paddingLength.get(District.LEFT),
                 config.paddingLength.get(District.DOWN),
                 districts.get(PADDING).get(LEFT).startInBlockX(),
                 districts.get(PADDING).get(LEFT).endInBlockY()));
@@ -155,10 +156,44 @@ public class Barcode {
                 config.paddingLength.get(District.DOWN),
                 districts.get(PADDING).get(RIGHT).startInBlockX(),
                 districts.get(PADDING).get(RIGHT).endInBlockY()));
-        districts.get(MAIN_DISTRICT).set(MAIN_ZONE,new Zone(config.mainWidth,
-                config.mainHeight,
+
+        districts.get(META).set(LEFT_UP,new Zone(config.metaLength.get(District.LEFT),
+                config.metaLength.get(District.UP),
                 districts.get(PADDING).get(LEFT).endInBlockX(),
                 districts.get(PADDING).get(UP).endInBlockY()));
+        districts.get(META).set(UP,new Zone(config.mainWidth,
+                config.metaLength.get(District.UP),
+                districts.get(META).get(LEFT_UP).endInBlockX(),
+                districts.get(META).get(LEFT_UP).startInBlockY()));
+        districts.get(META).set(LEFT,new Zone(config.metaLength.get(District.LEFT),
+                config.mainHeight,
+                districts.get(META).get(LEFT_UP).startInBlockX(),
+                districts.get(META).get(LEFT_UP).endInBlockY()));
+        districts.get(META).set(LEFT_DOWN,new Zone(config.metaLength.get(District.LEFT),
+                config.metaLength.get(District.DOWN),
+                districts.get(META).get(LEFT).startInBlockX(),
+                districts.get(META).get(LEFT).endInBlockY()));
+        districts.get(META).set(DOWN,new Zone(districts.get(PADDING).get(UP).widthInBlock,
+                config.metaLength.get(District.DOWN),
+                districts.get(META).get(LEFT_DOWN).endInBlockX(),
+                districts.get(META).get(LEFT_DOWN).startInBlockY()));
+        districts.get(META).set(RIGHT_UP,new Zone(config.metaLength.get(District.RIGHT),
+                config.metaLength.get(District.UP),
+                districts.get(META).get(UP).endInBlockX(),
+                districts.get(META).get(UP).startInBlockY()));
+        districts.get(META).set(RIGHT,new Zone(config.metaLength.get(District.RIGHT),
+                districts.get(META).get(LEFT).heightInBlock,
+                districts.get(META).get(RIGHT_UP).startInBlockX(),
+                districts.get(META).get(RIGHT_UP).endInBlockY()));
+        districts.get(META).set(RIGHT_DOWN,new Zone(config.metaLength.get(District.RIGHT),
+                config.metaLength.get(District.DOWN),
+                districts.get(META).get(RIGHT).startInBlockX(),
+                districts.get(META).get(RIGHT).endInBlockY()));
+
+        districts.get(MAIN_DISTRICT).set(MAIN_ZONE,new Zone(config.mainWidth,
+                config.mainHeight,
+                districts.get(META).get(LEFT).endInBlockX(),
+                districts.get(META).get(UP).endInBlockY()));
 
         int[] parts=new int[]{District.LEFT,District.UP,District.RIGHT,District.DOWN,
                 District.LEFT_UP,District.RIGHT_UP,District.RIGHT_DOWN,District.LEFT_DOWN};
@@ -166,10 +201,12 @@ public class Barcode {
             districts.get(Districts.MARGIN).get(part).addBlock(config.marginBlock.get(part));
             districts.get(Districts.BORDER).get(part).addBlock(config.borderBlock.get(part));
             districts.get(Districts.PADDING).get(part).addBlock(config.paddingBlock.get(part));
+            districts.get(Districts.META).get(part).addBlock(config.metaBlock.get(part));
 
             districts.get(Districts.MARGIN).get(part).addContent(config.marginContent.get(part));
             districts.get(Districts.BORDER).get(part).addContent(config.borderContent.get(part));
             districts.get(Districts.PADDING).get(part).addContent(config.paddingContent.get(part));
+            districts.get(Districts.META).get(part).addContent(config.metaContent.get(part));
         }
         districts.get(Districts.MAIN).get(District.MAIN).addBlock(config.mainBlock.get(District.MAIN));
     }
--- src/BarcodeConfig.java
+++ src/BarcodeConfig.java
@@ -11,6 +11,9 @@ public class BarcodeConfig {
     public DistrictConfig<Integer> paddingLength=new DistrictConfig<>(1);
     //public DistrictConfig<Integer> paddingLength=new DistrictConfig<>(1,1,1,1);
 
+    public DistrictConfig<Integer> metaLength=new DistrictConfig<>(1);
+    //public DistrictConfig<Integer> metaLength=new DistrictConfig<>(1,1,1,1);
+
     public int mainWidth=8;
     public int mainHeight=8;
 
@@ -52,6 +55,18 @@ public class BarcodeConfig {
             new BlackWhiteBlock());
     */
 
+    public DistrictConfig<Block> metaBlock=new DistrictConfig<>(new BlackWhiteBlock(CustomColor.Y0UmVm,CustomColor.Y1UmVm));
+    /*
+    public DistrictConfig<Block> metaBlock=new DistrictConfig<>(new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock(),
+            new BlackWhiteBlock());
+    */
+
     public DistrictConfig<Block> mainBlock=new DistrictConfig<>(new BlackWhiteBlock(CustomColor.Y0UmVm,CustomColor.Y1UmVm));
 
     public DistrictConfig<BitContent> marginContent=new DistrictConfig<>(new BitContent(BitContent.ALL_ONES));
@@ -81,6 +96,18 @@ public class BarcodeConfig {
     public DistrictConfig<BitContent> paddingContent=new DistrictConfig<>(new BitContent(BitContent.ALL_ONES));
     /*
     public DistrictConfig<BitContent> paddingContent=new DistrictConfig<>(null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    */
+
+    public DistrictConfig<BitContent> metaContent=new DistrictConfig<>(new BitContent(BitContent.ALL_ONES));
+    /*
+    public DistrictConfig<BitContent> metaContent=new DistrictConfig<>(null,
             null,
             null,
             null,
