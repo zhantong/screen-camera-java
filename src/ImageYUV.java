7cce3f013afb53f9c6d3dfbcba7a885f9284cb31
Tong ZHAN <zhantong1994@163.com>
2017-05-08 22:52:06 +0800

    修正YUV444和YUV420输出

--- src/ImageYUV.java
+++ src/ImageYUV.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
  */
 public class ImageYUV implements Image{
     public static final int TYPE_YUV444=0;
+    public static final int TYPE_YUV420=1;
     private byte[] YBuffer;
     private byte[] UBuffer;
     private byte[] VBuffer;
@@ -76,24 +77,33 @@ public class ImageYUV implements Image{
             case TYPE_YUV444:
                 colorName="YUV444P";
                 break;
+            case TYPE_YUV420:
+                colorName="YUV420P";
+                break;
             default:
                 throw new IllegalArgumentException();
         }
-        byte[] newU=new byte[width*height/4];
-        byte[] newV=new byte[width*height/4];
-        for(int y=0;y<height/2;y++){
-            int startYPos=y*width/2;
-            for(int x=0;x<width/2;x++){
-                newU[startYPos+x]=(byte)((UBuffer[(y*2)*width+x*2]+UBuffer[(y*2)*width+x*2+1]+UBuffer[(y*2+1)*width+x*2]+UBuffer[(y*2+1)*width+x*2+1])/4);
-                newV[startYPos+x]=(byte)((VBuffer[(y*2)*width+x*2]+VBuffer[(y*2)*width+x*2+1]+VBuffer[(y*2+1)*width+x*2]+VBuffer[(y*2+1)*width+x*2+1])/4);
-            }
-        }
+
         String fileName=String.format("%s_%dx%d_%06d.yuv",colorName,width,height,index);
         String filePath=Utils.combinePaths(directoryPath,fileName);
         FileOutputStream fos=new FileOutputStream(filePath);
         fos.write(YBuffer);
-        fos.write(newU);
-        fos.write(newV);
+        if(colorType==TYPE_YUV444){
+            fos.write(UBuffer);
+            fos.write(VBuffer);
+        }else if(colorType==TYPE_YUV420){
+            byte[] newU=new byte[width*height/4];
+            byte[] newV=new byte[width*height/4];
+            for(int y=0;y<height/2;y++){
+                int startYPos=y*width/2;
+                for(int x=0;x<width/2;x++){
+                    newU[startYPos+x]=(byte)((UBuffer[(y*2)*width+x*2]+UBuffer[(y*2)*width+x*2+1]+UBuffer[(y*2+1)*width+x*2]+UBuffer[(y*2+1)*width+x*2+1])/4);
+                    newV[startYPos+x]=(byte)((VBuffer[(y*2)*width+x*2]+VBuffer[(y*2)*width+x*2+1]+VBuffer[(y*2+1)*width+x*2]+VBuffer[(y*2+1)*width+x*2+1])/4);
+                }
+            }
+            fos.write(newU);
+            fos.write(newV);
+        }
         fos.close();
     }
 }
