1640eb962e682032ee513a45a1d9b7d100e418b6
Tong ZHAN <zhantong1994@163.com>
2017-05-17 08:07:09 +0800

    修正ColorBlock颜色选择功能

--- src/ColorBlock.java
+++ src/ColorBlock.java
@@ -5,19 +5,14 @@ public class ColorBlock implements Block {
     private int bitsPerUnit;
     private CustomColor[] mColors;
     public ColorBlock(int bitsPerUnit,CustomColor[] colors){
+        if(colors.length!=Math.pow(2,bitsPerUnit)){
+            throw new IllegalArgumentException();
+        }
         this.bitsPerUnit=bitsPerUnit;
         mColors=colors;
     }
     public void draw(Image image, int x, int y, int width, int height, int value, int barcodeIndex, int column, int row) {
-        CustomColor color=null;
-        switch (value){
-            case 0:
-                color=mColors[0];
-                break;
-            case 1:
-                color=mColors[1];
-                break;
-        }
+        CustomColor color=mColors[value];
         image.fillRect(x,y,width,height,color);
     }
 
