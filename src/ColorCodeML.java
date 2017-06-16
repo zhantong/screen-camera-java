3e838ae37b90f87bd2cafdbf4949990f397a05f9
Tong ZHAN <zhantong1994@163.com>
2017-06-16 21:48:04 +0800

    优化numRandomBarcode的使用 由成员变量调整为EncodeHint

--- src/ColorCodeML.java
+++ src/ColorCodeML.java
@@ -10,8 +10,9 @@ public class ColorCodeML extends BlackWhiteCodeML {
         hints.put(EncodeHintType.RS_ERROR_CORRECTION_SIZE,12);
         hints.put(EncodeHintType.RS_ERROR_CORRECTION_LEVEL,0.1);
         hints.put(EncodeHintType.RAPTORQ_NUMBER_OF_SOURCE_BLOCKS,1);
+        hints.put(EncodeHintType.NUMBER_OF_RANDOM_BARCODES,100);
         ColorCodeML colorCodeML=new ColorCodeML(new ColorCodeMLConfig(),hints);
-        colorCodeML.toImages("/Volumes/扩展存储/ShiftCode实验/发送方/sample2.txt","/Users/zhantong/Desktop/ColorCodeML_40x40_0.1");
+        colorCodeML.toImages("/Volumes/扩展存储/ShiftCode实验/发送方/sample0.txt","/Users/zhantong/Desktop/ColorCodeML");
     }
     public ColorCodeML(BarcodeConfig config, Map<EncodeHintType, ?> hints) {
         super(config, hints);
